fe9cb303ce287b500b7b68649150fae54ba7af72
Kent <asilo.kentbryanv19@gmail.com>
2025-02-24 17:02:12 +0800

    feature/ recovery code page

--- app/(pages)/(authentication)/forgot-password/page.tsx
+++ app/(pages)/(authentication)/forgot-password/page.tsx
@@ -8,13 +8,7 @@ import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
 import { IoArrowBack } from "react-icons/io5";
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-
-} from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
 
 const formSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
@@ -22,30 +16,30 @@ const formSchema = z.object({
 
 type FormValues = z.infer<typeof formSchema>;
 
-
-export default function forgotPassword() {
+export default function ForgotPassword() {
   const router = useRouter();
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      email: ""
+      email: "",
     },
   });
 
   const onSubmit = (values: FormValues) => {
     console.log("Form data:", values);
     router.push("/reset-code");
-
   };
 
   return (
     <section className="content w-full h-screen mx-auto bg-[#18181B] text-[#FAFAFA] flex justify-center items-center">
-      
       <div className="flex h-[275px] w-[408px] border-[1px] rounded-xl bg-[#18181B] border-[#84CC16] flex-col">
-      <div className="title flex flex-row h-auto p-6 pb-0 items-center gap-[10px] cursor-pointer" onClick={() => router.back()}>
+        <div
+          className="title flex flex-row h-auto p-6 pb-0 items-center gap-[10px] cursor-pointer"
+          onClick={() => router.back()}
+        >
           <IoArrowBack className="text-[20px] text-zinc-400" />
           <p className="text-[14px] mt-1 text-zinc-400 font-normal">Go back</p>
-      </div>
+        </div>
         <div className="form text-[14px] p-6 pb-0 pt-4">
           <div className="text pb-6">
             <h2 className="text-2xl font-semibold">Forgot Password</h2>
@@ -72,7 +66,7 @@ export default function forgotPassword() {
                   </FormItem>
                 )}
               />
-             
+
               <div className="Request-btn pt-5">
                 <Button type="submit" className="bg-[#84CC16] w-full">
                   Request reset code
@@ -81,7 +75,6 @@ export default function forgotPassword() {
             </form>
           </Form>
         </div>
-       
       </div>
     </section>
   );
