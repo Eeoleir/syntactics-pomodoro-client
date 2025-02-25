369f2a634510a713b759a6ec96bc05af51dd82c1
Kent <asilo.kentbryanv19@gmail.com>
2025-02-25 09:28:32 +0800

    feature/ new password page

--- app/(pages)/(authentication)/new-password/page.tsx
+++ app/(pages)/(authentication)/new-password/page.tsx
@@ -12,18 +12,24 @@ import { ResendTimer } from "@/components/custom/ResendTimer";
 import Link from "next/link";
 
 
-const formSchema = z.object({
-  recoveryCode: z.string().min(6),
-});
+const formSchema = z
+  .object({
+    password: z.string().min(6),
+    repassword: z.string().min(6),
+  })
+  .refine((data) => data.password === data.repassword, {
+    path: ["repassword"],
+  });
 
-type FormValues = z.infer<typeof formSchema>;
+  type FormValues = z.infer<typeof formSchema>;
 
-export default function ResetCode() {
+export default function NewPassword() {
   const router = useRouter();
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      recoveryCode: "",
+      password: "",
+      repassword: "",
     },
   });
 
@@ -34,7 +40,7 @@ export default function ResetCode() {
 
   return (
     <section className="content w-full h-screen mx-auto bg-[#18181B] text-[#FAFAFA] flex justify-center items-center flex-col">
-      <div className="flex h-[384px] w-[408px] border-[1px] rounded-xl bg-[#18181B] border-[#84CC16] flex-col">
+      <div className="flex h-[368px] w-[408px] border-[1px] rounded-xl bg-[#18181B] border-[#84CC16] flex-col">
         <div
           className="title flex flex-row h-auto p-6 pb-0 items-center gap-[10px] cursor-pointer"
           onClick={() => router.back()}
@@ -43,52 +49,61 @@ export default function ResetCode() {
           <p className="text-[14px] mt-1 text-zinc-400 font-normal">Go back</p>
         </div>
         <div className="form text-[14px] p-6 pb-0 pt-6">
-          <div className="text pb-6">
-            <h2 className="text-2xl font-semibold">Enter Reset Code</h2>
-            <h3 className="text-zinc-400 text-[16px] font-[700] pt-6">
-              Check email for reset link
-            </h3>
-            <p className="text-zinc-400">
-              Check the inbox of your email account, and input the reset code
-              provided
+          <div className="text pb-6 space-y-[10px]">
+            <h2 className="text-2xl font-semibold">New Password</h2>
+            <p className="text-zinc-400 text-[16px] font-[700]">
+            Please create a new password that you don’t use  on any other site.
             </p>
           </div>
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-3 text-left"
+              className="space-y-4 text-left"
             >
               <FormField
                 control={form.control}
-                name="recoveryCode"
+                name="password"
                 render={({ field }) => (
                   <FormItem>
                     <FormControl>
                       <Input
+                        type="password"
                         required
                         className="w-full bg-[#3D4142] border-none px-3 py-1"
-                        placeholder="Enter reset code"
+                        placeholder="Create new password"
                         {...field}
                       />
                     </FormControl>
                   </FormItem>
                 )}
               />
+              <FormField
+                control={form.control}
+                name="repassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        required
+                        className="w-full bg-[#3D4142] border-none px-3 py-1"
+                        placeholder="Confirm new password"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </form>
 
-              <div className="Request-btn pt-5">
+            <div className="Request-btn py-6">
                 <Button type="submit" className="bg-[#84CC16] w-full">
-                  <Link href="/new-password">Continue</Link>
+                  <Link href="/login">Continue</Link>
                 </Button>
               </div>
-            </form>
           </Form>
         </div>
-        <div className="links text-zinc-400 p-6 flex flex-col items-center text-sm space-y-1">
-          <h3 className="flex flex-row gap-2">
-            Didn’t receive an email? 
-            <ResendTimer />
-          </h3>
-        </div>
+        
       </div>
     </section>
   );
